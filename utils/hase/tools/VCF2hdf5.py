f6051cb8c1cf46fd8287646dc2520457093f284c
arnovanhilten <a.vanhilten@erasmusmc.nl>
2020-08-25 14:59:23 +0200

    changed import vcf2hdf5

--- utils/hase/tools/VCF2hdf5.py
+++ utils/hase/tools/VCF2hdf5.py
@@ -1,14 +1,13 @@
 import os
 import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from utils.hase.config import PYTHON_PATH
+from config import PYTHON_PATH
 if PYTHON_PATH is not None:
     for i in PYTHON_PATH: sys.path.insert(0, i)
 import argparse
 import h5py
 import pandas as pd
 import numpy as np
-from utils.hase.hdgwas.tools import Timer
 import tables
 import glob
 
