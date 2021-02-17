8758e1461050f6713542e02d8e80f0f1e641dc0c
arnovanhilten <a.vanhilten@erasmusmc.nl>
2021-02-17 12:07:39 +0100

    minor change convert

--- GenNet_utils/submit_SLURM_job.sh
+++ GenNet_utils/submit_SLURM_job.sh
@@ -4,7 +4,10 @@
 #SBATCH -o ./GenNet_utils/SLURM_logs/out_%j.log
 #SBATCH -e ./GenNet_utils/SLURM_logs/error_%j.log
 
+## MAKE SURE THIS IS EXECUTABLE! use chmod u+x submit_SLURM_job.sh
 # Load the modules
 module load 2019
+# activate venv
 source $HOME/venv_GenNet_dev/bin/activate
+# run conversion
 python ./GenNet_utils/Convert.py -job_begins $1 -job_tills $2 -job_n $3 -study_name $4 -outfolder $5 -tcm $6
