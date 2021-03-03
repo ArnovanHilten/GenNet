d5a7b58cb87f3f0f3d411f0100fbcbf56c0cb2e1
arnovanhilten <a.vanhilten@erasmusmc.nl>
2021-03-03 13:50:04 +0100

    Create networks from npz, add local multi jobs conversion

--- GenNet_utils/Utility_functions.py
+++ GenNet_utils/Utility_functions.py
@@ -160,36 +160,3 @@ def create_importance_csv(datapath, model, masks):
     total_list = total_list.merge(coordinates, on="node_layer_" + str(i))
     total_list["raw_importance"] = total_list.filter(like="weights").prod(axis=1)
     return total_list
-
-
-def query_yes_no(question, default="yes"):
-    """Ask a yes/no question via raw_input() and return their answer.
-
-    "question" is a string that is presented to the user.
-    "default" is the presumed answer if the user just hits <Enter>.
-        It must be "yes" (the default), "no" or None (meaning
-        an answer is required of the user).
-
-    The "answer" return value is True for "yes" or False for "no".
-    """
-    valid = {"yes": True, "y": True, "ye": True,
-             "no": False, "n": False}
-    if default is None:
-        prompt = " [y/n] "
-    elif default == "yes":
-        prompt = " [Y/n] "
-    elif default == "no":
-        prompt = " [y/N] "
-    else:
-        raise ValueError("invalid default answer: '%s'" % default)
-
-    while True:
-        sys.stdout.write(question + prompt)
-        choice = input().lower()
-        if default is not None and choice == '':
-            return valid[default]
-        elif choice in valid:
-            return valid[choice]
-        else:
-            sys.stdout.write("Please respond with 'yes' or 'no' "
-                             "(or 'y' or 'n').\n")
