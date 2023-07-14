098c8d77e321af1f43bd2412f4f720d337339e66
arnovanhilten <a.vanhilten@erasmusmc.nl>
2023-07-14 17:57:17 +0200

    rename submission files

--- multi_height_submit.sh
+++
@@ -1,3 +0,0 @@
-sbatch submit_train.sh 400000 0.00001 64 0  gene_network_snp_gene_filters 1
-sbatch submit_train.sh 400001 0.00001 64 0  gene_network_snp_gene_filters 2 
-sbatch submit_train.sh 400002 0.00001 64 0  gene_network_snp_gene_filters 3
